758cc510d74dbc652a0e2d2d3af63ec21586ec97
sshilyaev <shilyaev.sergey@ya.ru>
2026-03-09 13:06:33 +0700

    Initial Commit

---
+++ bodyMetric/bodyMetricApp.swift
@@ -0,0 +1,17 @@
+//
+//  bodyMetricApp.swift
+//  bodyMetric
+//
+//  Created by Sergey Shilyaev on 09.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct bodyMetricApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
